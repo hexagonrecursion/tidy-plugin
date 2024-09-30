c465204faedd9b385b7511b4b527bdcf96385e1a
hexagon-recursion <hexagonrecursion@gmail.com>
2024-09-30 16:42:41 +0300

    Use lit

--- build-and-test.sh
+++ build-and-test.sh
@@ -2,6 +2,7 @@ set -eu
 
 # apt update
 # apt install clang-18 clang-format-18 clang-tidy-18 libclang-18-dev llvm-18-dev ninja-build clang cmake
+# pipx install lit
 cmake -B build -G Ninja -S .
 cmake --build build/
-clang-tidy-18 --checks=colobot-str2path --load build/libStrToPathCheck.so test.cpp -- -std=c++20 2>&1 | /lib/llvm-18/bin/FileCheck test.cpp
+lit -v build/test/
